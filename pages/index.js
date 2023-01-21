d5d34608f6eccc979f93cf8e2a9cbf1f219ca764
Maykel Delgado <maykeld@outlook.com>
2023-01-21 17:43:09 +0000

    working semantic bible

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,11 @@ import styles from "./index.module.css";
 export default function Home() {
   const [input, setinput] = useState("");
   const [result, setResult] = useState();
+  const [error, setError] = useState(null);
 
   async function onSubmit(event) {
     event.preventDefault();
+    setError(null);
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
@@ -21,27 +23,53 @@ export default function Home() {
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
-
-      console.log(data);
+      
       setinput("");
-    } catch(error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
+
+      try {
+        const response = await fetch("/api/match", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ vector: data.result }),
+        });
+  
+        const matchData = await response.json();
+        if (response.status !== 200) {
+          throw matchData.error || new Error(`Request failed with status ${response.status}`);
+        }
+
+        // Get the div to place the results
+        let resultdiv = document.getElementById("result");
+        // Delete all content already inside
+        resultdiv.innerHTML = '';
+        
+        // Append the matches to the results div
+        matchData.result.forEach((element) => {
+          let p = document.createElement("p");
+          p.innerHTML = element.metadata.text;;
+          resultdiv.appendChild(p);
+        });
+      } catch (error) {
+        setError(error);
+      }
+      
+    } catch (error) {
+      setError(error);
     }
   }
-
 
   return (
     <div>
       <Head>
         <title>Semantic Bible</title>
-        <link rel="icon" href="/dog.png" />
+        <link rel="icon" href="/bible-icon.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Search</h3>
+        <img src="/bible-icon.png" className={styles.icon} />
+        <h3>Semantic Bible</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
@@ -52,7 +80,8 @@ export default function Home() {
           />
           <input type="submit" value="Search" />
         </form>
-        <div className={styles.result}>{result}</div>
+        {error && <div className={styles.error}>{error.message}</div>}
+        <div id="result" className={styles.result}>{result}</div>
       </main>
     </div>
   );
